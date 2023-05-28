259241417cc03743162f9a37ec0157f28cf2af66
renukadevib <134880554+renukadevib@users.noreply.github.com>
2023-05-28 14:30:44 -0500

    Update streamlit_app.py
    Adding Icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new Diner Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Blue Berry Oatmeal')
-streamlit.text('Spinach Kale Smoothi')
+streamlit.text('🥣 Omega 3 Blue Berry Oatmeal')
+streamlit.text('🥗 Spinach Kale Smoothi')
 streamlit.text('Hard-boile Egg')
+streamlit.text('🥑 Avocado Toast')
