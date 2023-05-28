0e8da62e997a94625901b52357ce13c0440efd40
renukadevib <134880554+renukadevib@users.noreply.github.com>
2023-05-28 17:49:21 -0500

    Update streamlit_app.py
    Adding fruity vice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My parents new Diner Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 Blue Berry Oatmeal')
@@ -12,3 +13,5 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
