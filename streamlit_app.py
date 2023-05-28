a521982912c699f2ee8ea8b09c6fd6196341af44
renukadevib <134880554+renukadevib@users.noreply.github.com>
2023-05-28 14:28:34 -0500

    Update streamlit_app.py
    Adding Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new Diner Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Blue Berry Oatmeal')
+streamlit.text('Spinach Kale Smoothi')
+streamlit.text('Hard-boile Egg')
