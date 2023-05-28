df92a1eed5fc7112943bf9c530be79213687bd7b
renukadevib <134880554+renukadevib@users.noreply.github.com>
2023-05-28 14:35:11 -0500

    Update streamlit_app.py
    Adding Pandas to read file and display in streamline

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 streamlit.title('My parents new Diner Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 Blue Berry Oatmeal')
 streamlit.text('🥗 Spinach Kale Smoothi')
 streamlit.text('🐔 Hard-boile Egg')
 streamlit.text('🥑 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
