b97f38bb07c4b642c923e158b3a9fe0be2c202a7
renukadevib <134880554+renukadevib@users.noreply.github.com>
2023-05-28 17:54:49 -0500

    Update streamlit_app.py
    Commenting json response display

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
